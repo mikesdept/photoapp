b376a72d0cbfcb7a8da441c9670a55d1c886ef11
Sergei Mikevich <mikesdepartment@gmail.com>
2024-06-19 23:26:54 +0200

    add base UI on photo list fragment

--- presentation/src/main/java/mikes/dept/presentation/ui/photolist/PhotoListFragment.kt
+++ presentation/src/main/java/mikes/dept/presentation/ui/photolist/PhotoListFragment.kt
@@ -1,7 +1,20 @@
 package mikes.dept.presentation.ui.photolist
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.lifecycle.ViewModelProvider
+import mikes.dept.presentation.R
 import mikes.dept.presentation.di.core.SubcomponentProvider
 import mikes.dept.presentation.ui.core.BaseComposeFragment
 
@@ -15,8 +28,37 @@ class PhotoListFragment : BaseComposeFragment<PhotoListViewModel>() {
         viewModelProvider[PhotoListViewModelImpl::class.java]
 
     @Composable
-    override fun ComposeContent() {}
+    override fun ComposeContent() {
+        ComposeContentView(viewModel = viewModel)
+    }
 
     override fun setup() {}
+
+    @Composable
+    private fun ComposeContentView(viewModel: PhotoListViewModel) {
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+                .background(color = Color.White)
+        ) {
+            Button(
+                onClick = { viewModel.onClickCreatePhoto() },
+                modifier = Modifier
+                    .align(Alignment.BottomCenter)
+                    .padding(bottom = 32.dp)
+            ) {
+                Text(
+                    text = stringResource(id = R.string.btn_create_photo),
+                    modifier = Modifier.padding(8.dp)
+                )
+            }
+        }
+    }
+
+    @Preview
+    @Composable
+    private fun ComposeContentPreview() {
+        ComposeContentView(viewModel = PhotoListViewModelComposable())
+    }
 
 }
--- presentation/src/main/java/mikes/dept/presentation/ui/photolist/PhotoListViewModel.kt
+++ presentation/src/main/java/mikes/dept/presentation/ui/photolist/PhotoListViewModel.kt
@@ -1,9 +1,36 @@
 package mikes.dept.presentation.ui.photolist
 
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flowOf
 import mikes.dept.presentation.ui.core.navdirections.NavDirectionsViewModel
 import mikes.dept.presentation.ui.core.navdirections.NavDirectionsViewModelImpl
+import mikes.dept.presentation.ui.core.navdirections.event.ErrorEvent
+import mikes.dept.presentation.ui.core.navdirections.event.NavDirectionsEvent
 import javax.inject.Inject
 
-interface PhotoListViewModel : NavDirectionsViewModel
+interface PhotoListViewModel : NavDirectionsViewModel {
 
-class PhotoListViewModelImpl @Inject constructor() : NavDirectionsViewModelImpl(), PhotoListViewModel
+    fun onClickCreatePhoto()
+
+}
+
+class PhotoListViewModelImpl @Inject constructor() : NavDirectionsViewModelImpl(), PhotoListViewModel {
+
+    override fun onClickCreatePhoto() {
+        val navDirectionsEvent = NavDirectionsEvent.Directions(
+            navDirections = PhotoListFragmentDirections.navPhotoCreateFragment()
+        )
+        navigate(navDirectionsEvent = navDirectionsEvent)
+    }
+
+}
+
+class PhotoListViewModelComposable : PhotoListViewModel {
+
+    override val navDirections: Flow<NavDirectionsEvent> = flowOf()
+
+    override val error: Flow<ErrorEvent> = flowOf()
+
+    override fun onClickCreatePhoto() {}
+
+}
